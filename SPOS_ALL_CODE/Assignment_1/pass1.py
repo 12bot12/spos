8440702ab40a8693439e14c9a15550b1f51b75ca
12bot12 <106923562+12bot12@users.noreply.github.com>
2022-06-05 22:22:10 +0530

    Update pass1.py

--- SPOS_ALL_CODE/Assignment_1/pass1.py
+++ SPOS_ALL_CODE/Assignment_1/pass1.py
@@ -1,108 +1,151 @@
+mnemonics={
+    'STOP':('00','IS',0),
+    'ADD':('01','IS',2),
+    'SUB':('02','IS',2),
+    'MUL':('03','IS',2),
+    'MOVER':('04','IS',2),
+    'MOVEM':('05','IS',2),
+    'COMP':('06','IS',2),
+    'BC':('07','IS',2),
+    'DIV':('08','IS',2),
+    'READ':('09','IS',1),
+    'PRINT':('10','IS',1),
+    'LTORG':('05','AD',0),
+    'ORIGIN':('03','AD',1),
+    'START':('01','AD',1),
+    'EQU':('04','AD',2),
+    'DS':('01','DL',1),
+    'DC':('02','DL',1),
+    'END':('AD',0)
+    }
+REG={'AREG':1,'BREG':2,'CREG':3,'DREG':4} 
 
-import os
-LC=0  #location counter initialization
-mnemonics={'STOP':('00','IS',0),'ADD':('01','IS',2),'SUB':('02','IS',2),'MUL':('03','IS',2),'MOVER':('04','IS',2),'MOVEM':('05','IS',2),'COMP':('06','IS',2),'BC':('07','IS',2),'DIV':('08','IS',2),'READ':('09','IS',1),'PRINT':('10','IS',1),'LTORG':('05','AD',0),'ORIGIN':('03','AD',1),'START':('01','AD',1),'EQU':('04','AD',2),'DS':('01','DL',1),'DC':('02','DL',1),'END':('AD',0)}
-file=open("input.txt")  #Enter input file with complete path if not in same directory
+# open files
+inputFile = open("input.txt",'r')
+Mc_code = open("Inter_codes.txt","a+")
 
-ifp=open("inter_code.txt","a")  #output file having intermediate code
-ifp.truncate(0)                 #cleaning file to remove previous data
-REG={'AREG':1,'BREG':2,'CREG':3,'DREG':4}  #Registers
-lit=open("literals.txt","a+")    #literals and their address containing file
-lit.truncate(0)
-tmp=open("tmp.txt","a+")
-tmp.truncate(0)
-symtab={}   #Sybol Table
-pooltab=[]	#Pool Table
-words=[]
+# variable declaration
+LC = 0   #location Counter
+words =[]
+symtab ={}
+littab ={}
+litCount = 1
+c=1
 
-#prints literal table
-def littab():
-    print("literal table:")
-    lit.seek(0,0)
-    for x in lit:
-        print(x)
 
-#prints pool table
-def pooltab2():
-    global pooltab
-    print("Pool Table:")
-    print(pooltab)
-
-#prints symbol table
-def symbol():
+# function declarations 
+def OTHERS(mnemonic,k):
+    global words
+    global mnemonics 
     global symtab
-    print("Symbol Table:")
-    print(symtab)
-
-#handles END directive        
-def END():
-    global LC
-    pool=0
-    z=0
-    ifp.write("\t(AD,02)\n")
-    lit.seek(0,0)
-    for x in lit:
-        if "**" in x:
-            pool+=1
-            if pool==1:
-                pooltab.append(z)
-            y=x.split()
-            tmp.write(y[0]+"\t"+str(LC)+"\n")
-            LC+=1
-        else:
-            tmp.write(x)
-        z+=1
-    lit.truncate(0)
-    tmp.seek(0,0)
-    for x in tmp:
-        lit.write(x)
-    tmp.truncate(0)
-   
-        
-#handles LTORG mnemonic
-def LTORG():
-    global LC
-    pool=0
-    z=0
-    lit.seek(0,0)
-    x=lit.readlines()
+    global LC,symindex,litCount
+    z=mnemonics[mnemonic]
+    Mc_code.write("\t("+z[1]+","+z[0]+")\t")
     i=0
-    while(i<len(x)):
-        f=[]
-        if("**" in x[i]):
-            j=0
-            pool+=1
-            if pool==1:
-                pooltab.append(z)
-            while(x[i][j]!="'"):
-                j+=1
-            j+=1
-            while(x[i][j]!="'"):
-                f.append(x[i][j])
-                j+=1
-            if(i!=len(x)-1):
-                ifp.write("\t(AD,05)\t(DL,02)(C,"+str(f[0])+")\n")
-                y=x[i].split()
-                tmp.write(y[0]+"\t"+str(LC)+"\n")
-                LC+=1
-                ifp.write(str(LC))
-            else:
-                ifp.write("\t(AD,05)\t(DL,02)(C,"+str(f[0])+")\n")
-                y=x[i].split()
-                tmp.write(y[0]+"\t"+str(LC)+"\n")
-                LC+=1
+    y=z[-1]
+    #print("y="+str(y))
+    for i in range(1,y+1):
+        words[k+i]=words[k+i].replace(",","")
+        if(words[k+i] in REG.keys()):
+            Mc_code.write("("+str(REG[words[k+i]])+")")
+        elif("=" in words[k+i]):
+            #print(words[k+i])
+            littab[words[k+i]] = [litCount,words[k+i],"**"]
+            #print(len(x))
+            Mc_code.write("(L,"+str(litCount)+")")
+            litCount+=1
         else:
-            tmp.write(x[i])
-        z+=1
-        i+=1
-    lit.truncate(0)
-    tmp.seek(0,0)
-    for x in tmp:
-        lit.write(x)
-    tmp.truncate(0)
-          
+            #print(words,symtab)
+            if(words[k+i] not in symtab.keys()):
+                symtab[words[k+i]]=(symindex,words[k+i],"**")
+                Mc_code.write("(S,"+str(symindex)+")")
+                symindex+=1
+            else:
+                w=symtab[words[k+i]]
+                Mc_code.write("(S,"+str(w[-1])+")")
+    #print(symtab)
+    Mc_code.write("\n")
+    LC+=1
 
-#handles ORIGIN mnemonic
+
+
+
+
+def detect_mnemonic(k):
+    global words,LC,c
+
+    if words[k]=="START":
+        LC=int(words[1])
+        Mc_code.write("\t(AD,01)\t(C,"+str(LC)+')\n')
+    elif words[k]=="DC":
+        Mc_code.write("\t(DL,02)\t(C,"+words[k+1]+")\n")
+        LC+=1
+    elif words[k] == "DS":
+        Mc_code.write("\t(DL,01)\t(C,"+words[k+1]+")\n")
+        LC=LC+int(words[k+1])
+    elif words[k]=="ORIGIN":
+        Mc_code.write("\t(AD,03)\t(C,"+str(words[k+1])+")\n")
+        LC =int(words[k+1])
+    elif words[k] == "END":
+         Mc_code.write("\t(AD,02) (c,"+str(c)+")\n")   
+         c+=1
+         for x in littab.values():
+             if "**" in x[2]:
+                 x[2]=LC
+                 LC+=1
+
+    else:
+        OTHERS(words[k],k)  
+
+
+# actual execution
+symindex=0
+for line in inputFile:
+    # print(line)
+    words = line.split()
+    # print(words)
+    if(LC > 0):
+        Mc_code.write(str(LC))
+        
+    k=0
+    # if first word is mnemonic
+    if words[0] in mnemonics.keys():
+        
+        # print(val)
+        k=0
+        detect_mnemonic(k)
+    # else it is a symbol
+    else:
+        # it is not present in symbol table then add it
+        if words[0] not in symtab.keys():
+            symtab[words[k]] = (symindex,words[k],LC)
+            symindex+=1
+            # symbol()  # to print symbol table
+
+        # present in symbol table
+        else:
+
+             x=symtab[words[k]]
+             if x[2]=="**":
+                symtab[words[k]]=(x[0],x[1],LC)
+            
+        k=1
+        detect_mnemonic(k)
+
+l = open("littab.txt",'w')
+s = open("symtab.txt",'w')
+
+for x in symtab.values():
+    s.write(str(x[0])+"\t  "+str(x[1])+"\t  "+str(x[2])+"\n")
+for x in littab.values():
+    l.write(str(x[0])+"\t  "+str(x[1])+"\t  "+str(x[2])+"\n")
+
+l.close()
+s.close()
+print(symtab)
+print(littab)
+inputFile.close()
+Mc_code.close()
 def ORIGIN(addr):
     global LC
     ifp.write("\t(AD,03)\t(C,"+str(addr)+")\n")
